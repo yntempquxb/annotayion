3eac39f4f04c278686efa3876482ba38bf43cdaa
yntempquxb <33142788+yntempquxb@users.noreply.github.com>
2019-06-16 23:23:08 +0800

    Add files via upload

---
+++ TsetController.java
@@ -0,0 +1,23 @@
+package com.lzp.annotation;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wangyong
+ *
+ * @Date: 2019/6/16.
+ * @Description:
+ */
+@RestController
+@RequestMapping("/test")
+public class TsetController {
+
+    @RequestMapping(value = "test",method = RequestMethod.POST)
+    @WebLog(description = "报案接口开始")
+    public void test(@RequestBody User user) {
+        System.out.println(user.getName() + "start");
+    }
+}
---
+++ User.java
@@ -0,0 +1,37 @@
+package com.lzp.annotation;
+
+/**
+ * Created by wangyong
+ *
+ * @Date: 2019/6/16.
+ * @Description:
+ */
+public class User {
+    private String name;
+    private String clz;
+    private String age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClz() {
+        return clz;
+    }
+
+    public void setClz(String clz) {
+        this.clz = clz;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
